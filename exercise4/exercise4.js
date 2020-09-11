c9c8a4b2bc174c8a7308dfafce54fcf2c2ad550d
DavidCardenas1 <cdcardenas@tekmexico.com>
2020-09-11 10:34:39 -0700

    exercise 4 selectorAll and selector for parents optimized

--- exercise4/exercise4.js
+++ exercise4/exercise4.js
@@ -6,25 +6,21 @@
 function querySelectorAll(query) {
   const result = [];
   const parentChilds = query.split('<');
-  query = query.replace('<', '>');
-  const findRootParent = function(elm, selector) {
-    elm.querySelectorAll(selector).forEach((current) => {
-      while (current && (elm !== current)) {
-        current = current.parentNode;
-      }
-      if (current) {
-        result.push(current);
-      }
-    });
+  const findRootParent = function (elm, selector) {
     if (!selector) {
       result.push(elm);
+      return
     }
+    let current = elm.querySelector(selector)
+    if (current) {
+      result.push(elm);
+    }    
   };
 
   document.querySelectorAll(parentChilds[0]).forEach((element) => {
     findRootParent(element, parentChilds[1]);
   });
-  // console.log(result);
+  // console.log(result);  
   return result;
 }
 
