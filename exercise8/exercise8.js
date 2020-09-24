36e21e6dda22b0c2c03ca6e11384aff7337c1dae
DavidCardenas1 <cdcardenas@tekmexico.com>
2020-09-23 18:05:08 -0700

    exercise 8 basic check

--- exercise8/exercise8.js
+++ exercise8/exercise8.js
@@ -4,32 +4,29 @@
  * @return {Number}
  */
 function balanceSum(arr) {
-  let pointLeft = 0; let pointRight = arr.length - 1;
-  let sumLeft = arr[0]; let sumRight = arr[arr.length - 1];
-  while (pointLeft < pointRight) {
-    if (sumLeft < sumRight) {
-      pointLeft++;
-      sumLeft = sumLeft + arr[pointLeft];
-      if ((pointLeft + 1) === pointRight&&sumRight===sumLeft) {
-        // console.log(pointLeft);
-        return pointLeft;
-      }
-      continue;
+    let pointLeft = 0; let pointRight = arr.length - 1;
+    let sumLeft = arr[0]; let sumRight = arr[arr.length - 1];
+    while (pointLeft < pointRight) {
+        if ((pointRight - 1) === pointLeft && sumRight === sumLeft) {
+            // console.log(pointLeft);
+            return pointLeft;
+        }
+        if (sumLeft < sumRight) {
+            pointLeft++;
+            sumLeft = sumLeft + arr[pointLeft];
+            if ((pointLeft + 1) === pointRight && sumRight === sumLeft) {
+                // console.log(pointLeft);
+                return pointLeft;
+            }
+            continue;
+        }
+        if (sumRight <= sumLeft) {
+            pointRight--;
+            sumRight = sumRight + arr[pointRight];
+        }
     }
-    if (sumRight <= sumLeft) {
-      pointRight--;
-      sumRight = sumRight + arr[pointRight];
-      if ((pointRight-1)===pointLeft&&sumRight===sumLeft) {
-        // console.log(pointLeft);
-        return pointLeft;
-      }
-    }
-  }
-  // console.log(-1);
-  return -1;
+    // console.log(-1);
+    return -1;
 }
-// const arr = [1, 2, 3, 4, 9, 9, 2, 7, 10, 13];
-// const arr2 = [1, 1, 2];
-// const arr=[15, 2, 3, 5, 1, 2, 2];
-// balanceSum(arr);
+// balanceSum([1, 1]);
 module.exports = balanceSum;
