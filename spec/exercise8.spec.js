06f19f354a060bfa773397b7eb15464bd86e445d
DavidCardenas1 <cdcardenas@tekmexico.com>
2020-09-23 17:23:36 -0700

    exercise 8 first commit

---
+++ spec/exercise8.spec.js
@@ -0,0 +1,43 @@
+const balanceSum = require('../exercise8/exercise8');
+describe('exercise 8', function() {
+  it('same value even', function() {
+    const arr = [1, 1, 1, 1];
+    const result = balanceSum(arr);
+    expect(result).toBe(1);
+  });
+  it('same value odd', function() {
+    const arr = [1, 1, 1, 1, 1];
+    const result = balanceSum(arr);
+    expect(result).toBe(-1);
+  });
+  it('differents values even balance', function() {
+    const arr = [1, 2, 3, 4, 9, 9, 2, 7, 10, 13];
+    const result = balanceSum(arr);
+    expect(result).toBe(6);
+  });
+  it('differents values even not balance', function() {
+    const arr = [1, 2, 3, 4, 9, 9, 2, 7, 10, 10];
+    const result = balanceSum(arr);
+    expect(result).toBe(-1);
+  });
+  it('differents values odd not balance', function() {
+    const arr = [1, 2, 3, 4, 9, 9, 2, 7, 10, 13, 2];
+    const result = balanceSum(arr);
+    expect(result).toBe(-1);
+  });
+  it('differents values odd balance', function() {
+    const arr = [1, 2, 3, 4, 9, 9, 2, 7, 23];
+    const result = balanceSum(arr);
+    expect(result).toBe(6);
+  });
+  it('balance in the first position', function() {
+    const arr = [15, 2, 3, 5, 1, 2, 2];
+    const result = balanceSum(arr);
+    expect(result).toBe(0);
+  });
+  it('balance in the last position', function() {
+    const arr = [2, 2, 3, 5, 1, 2, 15];
+    const result = balanceSum(arr);
+    expect(result).toBe(5);
+  });
+});
